73ad51bfb5899e6e52e7f98f9fb5d108593cd441
chiyu <3410526033@qq.com>
2026-07-06 21:54:15 +0800

    4

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,7 @@
 import pandas as pd
-REQUIRED_COLUMNS = {'数量','单价','产品','地区'}
+
 def analyze_excel(df: pd.DataFrame):
-    missing = REQUIRED_COLUMNS - set(df.columns)
-    if missing:
-        raise ValueError(f'缺少必需列：{missing}')
-    df = df.copy()
+    df = clean_data(df)
     df = calculate_sales(df)
     df_region = group_by_region(df)
     df_products = group_by_products(df)
@@ -30,4 +27,23 @@ def group_by_products(df: pd.DataFrame) -> pd.DataFrame:
     df_products = dg['销售额'].sum().reset_index()
     df_products.columns = ['产品','总销售额']
     return df_products
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    REQUIRED_COLUMNS = {'数量', '单价', '产品', '地区'}
+    df = df.copy()
+    df = df.dropna(how='all')
+    df = df.drop_duplicates()
+
+    missing = REQUIRED_COLUMNS - set(df.columns)
+    if missing:
+        raise ValueError(f'缺少必需列：{missing}')
+    df['数量'] = pd.to_numeric(df['数量'],errors='coerce')
+    df['单价'] = pd.to_numeric(df['单价'],errors='coerce')
+
+    df = df.dropna(subset=['数量', '单价'])
+    df["地区"] = df["地区"].fillna("未知")
+    df["地区"] = df["地区"].replace("", "未知")
+    df["地区"] = df["地区"].str.strip().replace("", "未知")
+    df = df[(df['数量']>0) & (df['单价']>0)]
+    df = df.drop_duplicates()
+    return df
 
